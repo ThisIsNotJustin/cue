e9e69dc26e9a2c3f34055e8d25ae23cbf303fa8a
ThisIsNotJustin <justincolemanonetwo@gmail.com>
2025-03-14 23:49:25 -0500

    alternative gamuts

--- cue.h
+++ cue.h
@@ -1,12 +1,37 @@
 #ifndef CUE_H
 #define CUE_H
 
+// https://developers.meethue.com/develop/application-design-guidance/color-conversion-formulas-rgb-to-xy-and-back/#Gamut
+// Gamut C
 #define RED_X 0.6915
 #define RED_Y 0.3038
 #define GREEN_X 0.17
 #define GREEN_Y 0.7
 #define BLUE_X 0.1532
 #define BLUE__Y 0.0475
+/*
+// Gamut B
+#define RED_X 0.675
+#define RED_Y 0.322
+#define GREEN_X 0.409
+#define GREEN_Y 0.518
+#define BLUE_X 0.167
+#define BLUE__Y 0.04
+// Gamut A
+#define RED_X 0.704
+#define RED_Y 0.296
+#define GREEN_X 0.2151
+#define GREEN_Y 0.7106
+#define BLUE_X 0.138
+#define BLUE__Y 0.08
+// Alt
+#define RED_X 1.0
+#define RED_Y 0.0
+#define GREEN_X 0.0
+#define GREEN_Y 1.0
+#define BLUE_X 0.0
+#define BLUE__Y 0.0
+*/
 
 const char *MAC;
 const char *POWER_UUID;
